b8b75b56803fadb50a2692c30d3933befec0964c
Vitor Melo <vitor.melo@cmsw.com>
2025-03-07 16:36:14 -0300

    update

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   let clock;
 
   // Grab the current date
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2023-10-29 12:00", "Asia/Kolkata");
+  let targetDate = moment.tz("2025-11-25 13:00", "UTC");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
@@ -18,30 +18,30 @@ $(document).ready(function() {
       autostart: false
     });
     console.log("Date has already passed!")
-    
+
   } else {
     // Run countdown timer
     clock = $(".clock").FlipClock(diff, {
       clockFace: "DailyCounter",
       countdown: true,
       callbacks: {
-        stop: function() {
+        stop: function () {
           console.log("Timer has ended!")
         }
       }
     });
-    
+
     // Check when timer reaches 0, then stop at 0
-    setTimeout(function() {
+    setTimeout(function () {
       checktime();
     }, 1000);
-    
+
     function checktime() {
       t = clock.getTime();
       if (t <= 0) {
         clock.setTime(0);
       }
-      setTimeout(function() {
+      setTimeout(function () {
         checktime();
       }, 1000);
     }
