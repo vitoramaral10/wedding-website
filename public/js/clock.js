68755f8bb85a2ee26dfc738c95b75571c4f8ad26
Vitor Melo <vitor.melo@cmsw.com>
2025-03-10 14:40:43 -0300

    Update target date in clock.js to reflect correct timezone and time

--- public/js/clock.js
+++ public/js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2025-11-25 13:00", "UTC");
+  let targetDate = moment.tz("2025-11-25 10:00:00", "America/Sao_Paulo");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
